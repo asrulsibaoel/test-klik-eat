1a980a38e6952d17a558034123c10577adae3e0b
asrul sani ariesandy <asrulsibaoel@gmail.com>
2017-03-08 01:11:04 +0700

    adding documentations

---
+++ public/documentations/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "API Doc Leader Board",
+  "version": "0.1.0",
+  "description": "API Documentation for Game Leader Board",
+  "title": "API Doc For Leader Board",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-07T18:09:30.442Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
